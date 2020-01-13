18524177f0c112190a6dd553182cda67172a5319
Kuncoro Wicaksono <coroo.wicaksono@gmail.com>
2020-01-13 11:14:46 +0700

    :sparkles: Add refresh button feature

--- resources/js/components/DoughnutChart.vue
+++ resources/js/components/DoughnutChart.vue
@@ -1,7 +1,13 @@
 <template>
     <card class="p-10">
-        <h4 class="chart-js-dashboard-title">{{ checkTitle }}</h4>
-        <line-chart :chart-data="datacollection" :options="options"></line-chart>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+      <div class="stay-right" v-show="buttonRefresh">
+        <button @click="fillData()" class="btn-refresh">
+          <i class="fas fa-sync"></i>
+        </button>
+      </div>
+      <h4 class="chart-js-dashboard-title">{{ checkTitle }}</h4>
+      <line-chart :chart-data="datacollection" :options="options"></line-chart>
     </card>
 </template>
 
@@ -16,6 +22,7 @@
       return {
         datacollection: null,
         options: null,
+        buttonRefresh: this.card.options.btnRefresh,
       }
     },
     computed: {
@@ -72,5 +79,37 @@
     font-weight: 800;
     margin: 0;
     padding: 1rem 1.5rem -10px;
+  }
+  .stay-right {
+    text-align: right;
+    width: 100%;
+    font-size: 12px;
+    padding: 5px;
+    margin-bottom: -40px;
+  }
+  .btn-refresh {
+    background-color: #e0e0e0;
+    color: #777;
+    padding: 5px;
+    border-radius: 5px;
+  }
+  .btn-refresh:hover {
+    color: #111;
+  }
+  .fa-sync:hover {
+    -webkit-animation: spin 2s infinite linear;
+    -moz-animation: spin 2s infinite linear;
+    -o-animation: spin 2s infinite linear;
+    animation: spin 2s infinite linear;
+  }
+
+  @-webkit-keyframes spin {
+    0% { -webkit-transform: rotate(0deg); }
+    100% { -webkit-transform: rotate(360deg); }
+  }
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
   }
 </style>
