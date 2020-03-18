6ed4249b3ce1e4b6a5acddbb8d2991790e22c979
Kuncoro Wicaksono <coroo.wicaksono@gmail.com>
2020-03-18 17:35:11 +0700

    :lipstick: change the padding for button

--- resources/js/components/BarChart.vue
+++ resources/js/components/BarChart.vue
@@ -2,9 +2,9 @@
     <card class="p-10">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
       <div class="stay-right">
-        <button @click="fillData()" class="btn-refresh" v-show="buttonRefresh">
+        <a @click="fillData()" class="btn-refresh" v-show="buttonRefresh">
           <i class="fas fa-sync"></i>
-        </button>
+        </a>
         <a :href="externalLink" :target="externalLinkIn" class="btn-external" v-show="btnExtLink">
           <i class="fas fa-external-link-alt"></i>
         </a>
@@ -219,7 +219,7 @@
     width: 100%;
     font-size: 12px;
     min-height: 35px;
-    padding: 5px;
+    padding: 10px 5px;
     margin-bottom: -40px;
   }
   .btn-refresh {
