0d6985cb8c0e3360d1b926e34285b8596b860343
adeve <antoine.deve91@gmail.com>
2023-11-20 11:50:16 +0100

    🐛 - Supports latest version of laravel/nova (>= 4.31)

--- src/api/TotalRecordsController.php
+++ src/api/TotalRecordsController.php
@@ -23,17 +23,21 @@ class TotalRecordsController extends Controller
         if ($request->input('model')) {
             $request->merge(['model' => urldecode($request->input('model'))]);
         }
-        $showTotal = isset($request->options) ? json_decode($request->options, true)['showTotal'] ?? true : true;
-        $totalLabel = isset($request->options) ? json_decode($request->options, true)['totalLabel'] ?? 'Total' : 'Total';
+
+        $options = is_string($request->options) ? json_decode($request->options, true) : $request->input('options', []);
+        $join = is_string($request->join) ? json_decode($request->join, true) : $request->input('join', []);
+
+        $showTotal = $options['showTotal'] ?? true;
+        $totalLabel = $options['totalLabel'] ?? 'Total';
         $chartType = $request->type ?? 'bar';
-        $advanceFilterSelected = isset($request->options) ? json_decode($request->options, true)['advanceFilterSelected'] ?? false : false;
-        $dataForLast = isset($request->options) ? json_decode($request->options, true)['latestData'] ?? 3 : 3;
-        $unitOfMeasurement = isset($request->options) ? json_decode($request->options, true)['uom'] ?? 'month' : 'month';
-        $startWeek = isset($request->options) ? json_decode($request->options, true)['startWeek'] ?? '1' : '1';
+        $advanceFilterSelected = $options['advanceFilterSelected'] ?? false;
+        $dataForLast = $options['latestData'] ?? 3;
+        $unitOfMeasurement = $options['uom'] ?? 'month';
+        $startWeek = $options['startWeek'] ?? '1';
         if(!in_array($unitOfMeasurement, ['day', 'week', 'month', 'hour'])){
             throw new ThrowError('UOM not defined correctly. <br/>Check documentation: https://github.com/coroo/nova-chartjs');
         }
-        $calculation = isset($request->options) ? json_decode($request->options, true)['sum'] ?? 1 : 1;
+        $calculation = $options['sum'] ?? 1;
         $request->validate(['model'   => ['bail', 'required', 'min:1', 'string']]);
         $model = $request->input('model');
         $modelInstance = new $model;
@@ -51,8 +55,8 @@ class TotalRecordsController extends Controller
             $defaultColor = array("rgba($brandColor, 1)", "#ffcc5c","#91e8e1","#ff6f69","#88d8b0","#b088d8","#d8b088", "#88b0d8", "#6f69ff","#7cb5ec","#434348","#90ed7d","#8085e9","#f7a35c","#f15c80","#e4d354","#2b908f","#f45b5b","#91e8e1","#E27D60","#85DCB","#E8A87C","#C38D9E","#41B3A3","#67c4a7","#992667","#ff4040","#ff7373","#d2d2d2");
             if(isset($request->series)){
                 foreach($request->series as $seriesKey => $serieslist){
-                    $seriesData = json_decode($serieslist);
-                    $filter = $seriesData->filter;
+                    $seriesData = (object) $serieslist;
+                    $filter = (object) $seriesData->filter;
                     $labelList[$seriesKey] = $seriesData->label;
                     if(empty($filter->value)&&isset($filter->operator)&&($filter->operator=='IS NULL' || $filter->operator=='IS NOT NULL')) {
                         $seriesSql .= ", SUM(CASE WHEN ".$filter->key." ".$filter->operator." then ".$calculation." else 0 end) as \"".$labelList[$seriesKey]."\"";
@@ -70,8 +74,8 @@ class TotalRecordsController extends Controller
                 }
             }
             if($unitOfMeasurement=='day'){
-                if(isset($request->join)){
-                    $joinInformation = json_decode($request->join, true);
+                if(count($join)){
+                    $joinInformation = $join;
                     $query = $model::selectRaw('DATE('.$xAxisColumn.') AS cat, DATE('.$xAxisColumn.') AS catorder, sum('.$calculation.') counted'.$seriesSql)
                         ->join($joinInformation['joinTable'], $joinInformation['joinColumnFirst'], $joinInformation['joinEqual'], $joinInformation['joinColumnSecond']);
                 } else {
@@ -96,8 +100,8 @@ class TotalRecordsController extends Controller
                 $query->groupBy('catorder', 'cat')
                     ->orderBy('catorder', 'asc');
             } else if($unitOfMeasurement=='week'){
-                if(isset($request->join)){
-                    $joinInformation = json_decode($request->join, true);
+                if(count($join)){
+                    $joinInformation = $join;
                     if($connectionName == 'pgsql'){
                         $query = $model::selectRaw("to_char(DATE_TRUNC('week', ".$xAxisColumn."), 'YYYYWW') AS cat, to_char(DATE_TRUNC('week', ".$xAxisColumn."), 'YYYYWW') AS catorder, sum(".$calculation.") counted".$seriesSql)
                             ->join($joinInformation['joinTable'], $joinInformation['joinColumnFirst'], $joinInformation['joinEqual'], $joinInformation['joinColumnSecond']);
@@ -131,8 +135,8 @@ class TotalRecordsController extends Controller
                 $query->groupBy('catorder', 'cat')
                     ->orderBy('catorder', 'asc');
             } else if($unitOfMeasurement=='hour'){
-                if(isset($request->join)){
-                    $joinInformation = json_decode($request->join, true);
+                if(count($join)){
+                    $joinInformation = $join;
                     $query = $model::selectRaw('HOUR('.$xAxisColumn.') AS cat, HOUR('.$xAxisColumn.') AS catorder, sum('.$calculation.') counted'.$seriesSql)
                         ->join($joinInformation['joinTable'], $joinInformation['joinColumnFirst'], $joinInformation['joinEqual'], $joinInformation['joinColumnSecond']);
                 } else {
@@ -158,8 +162,8 @@ class TotalRecordsController extends Controller
                     ->orderBy('catorder', 'asc');
 
             } else {
-                if(isset($request->join)){
-                    $joinInformation = json_decode($request->join, true);
+                if(count($join)){
+                    $joinInformation = $join;
                     if($connectionName == 'pgsql'){
                         $query = $model::selectRaw("to_char(".$xAxisColumn.", 'Mon YYYY') AS cat, to_char(".$xAxisColumn.", 'YYYY-MM') AS catorder, sum(".$calculation.") counted".$seriesSql)
                             ->join($joinInformation['joinTable'], $joinInformation['joinColumnFirst'], $joinInformation['joinEqual'], $joinInformation['joinColumnSecond']);
@@ -194,8 +198,8 @@ class TotalRecordsController extends Controller
                     ->orderBy('catorder', 'asc');
             }
 
-            if(isset(json_decode($request->options, true)['queryFilter'])){
-                $queryFilter = json_decode($request->options, true)['queryFilter'];
+            if($options['queryFilter'] ?? false){
+                $queryFilter = $options['queryFilter'];
                 foreach($queryFilter as $qF){
                     if(isset($qF['value']) && !is_array($qF['value'])){
                         if(isset($qF['operator'])){
@@ -233,8 +237,8 @@ class TotalRecordsController extends Controller
             if(isset($request->series)){
                 $countKey = 0;
                 foreach($request->series as $sKey => $sData){
-                    $dataSeries = json_decode($sData);
-                    $filter = $dataSeries->filter;
+                    $dataSeries = (object) $sData;
+                    $filter = (object) $dataSeries->filter;
                     $yAxis[$sKey]['label'] = $dataSeries->label;
                     if(isset($dataSeries->fill)){
                         if($dataSeries->fill==false){
